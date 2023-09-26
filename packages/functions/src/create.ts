f696c005dc99c914f1c554dc97997ef8d677ea52
Alex Fernandes <43734868+alexdpf@users.noreply.github.com>
2023-09-26 12:46:49 +1000

    Adding the final API

--- packages/functions/src/create.ts
+++ packages/functions/src/create.ts
@@ -1,11 +1,35 @@
-import AWS from "aws-sdk";
-import * as uuid from "uuid";
-import { APIGatewayProxyEvent } from "aws-lambda";
+//Refactoring code to make DRY and create reusable shared behaviours for app reliability abd maintainability
+import * as uuid from "uuid"; 
+import { Table } from "sst/node/table"; 
+import handler from "@notes/core/handler"; 
+import dynamoDb from "@notes/core/dynamodb";
 
-import { Table } from "sst/node/table";
+export const main = handler(async (event) => { 
+    let data = { content: "", 
+    attachment: "",
+};
+    if (event.body != null) { 
+        data = JSON.parse(event.body);
+}
 
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const params = { TableName: Table.Notes.tableName, 
+    Item: { 
+        // The attributes of the item to be created 
+        userId: "123", // The id of the author 
+        noteId: uuid.v1(), // A unique uuid 
+        content: data.content, // Parsed from request body 
+        attachment: data.attachment, // Parsed from request body 
+        createdAt: Date.now(), // Current Unix timestamp
+    }, 
+};
 
+await dynamoDb.put(params);
+
+return JSON.stringify(params.Item);
+});
+
+/*
+OLD Version
 export async function main(event: APIGatewayProxyEvent) {
     let data, params;
     console.log(event);
@@ -50,6 +74,7 @@ export async function main(event: APIGatewayProxyEvent) {
         };
     }
 }
+*/
 
-//
+//API Endpoint URL
 //https://s3vl847if3.execute-api.us-east-1.amazonaws.com
--- stacks/ApiStack.ts
+++ stacks/ApiStack.ts
@@ -12,7 +12,11 @@ const api = new Api(stack, "Api", {
         }, 
     },
     routes: {
+        "GET /notes": "packages/functions/src/list.main",
         "POST /notes": "packages/functions/src/create.main",
+        "GET /notes/{id}": "packages/functions/src/get.main",
+        "PUT /notes/{id}": "packages/functions/src/update.main",
+        "DELETE /notes/{id}": "packages/functions/src/delete.main",
     }, 
 });
 
